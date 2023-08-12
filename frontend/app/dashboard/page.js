3c9ed2b2e6eefc1a9e1694ebe5bc5c967988dac5
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-08-13 00:55:11 +0800

    Used ternary statement on sidebar breakpoint.

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Topbar from "@/app/global/Topbar";
-import SideBar from "@/app/global/Sidebar";
+import SideBar from "@/app/global/ProSidebar";
 import { ColorModeContext, useMode } from "@/app/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
