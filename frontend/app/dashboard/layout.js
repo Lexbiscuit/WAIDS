641159ac399499ef68d3cad5bcf92b44b76dd7ad
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-08-13 00:17:13 +0800

    Added layout for dashboard page.

---
+++ frontend/app/dashboard/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Dashboard",
+  //   Description for Login Page
+  description: "Dashboard Page for the WAIDS Web Application.",
+};
+
+export default function DashboardLayout({ children }) {
+  return <>{children}</>;
+}
