09b19c0d077a0d6b30c615c8201f033c0dae1a16
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-08-13 19:44:38 +0800

    Layouts cannot be client. TIL

--- frontend/app/login/layout.js
+++ frontend/app/login/layout.js
@@ -1,10 +1,3 @@
-"use client";
-import Topbar from "@/app/components/Topbar";
-import ProSidebar from "@/app/components/ProSidebar";
-import { ColorModeContext, useMode } from "@/app/theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { useMediaQuery } from "@mui/material";
-
 export const metadata = {
   title: "Login",
   //   Description for Login Page
@@ -12,23 +5,5 @@ export const metadata = {
 };
 
 export default function LoginLayout({ children }) {
-  const [theme, colorMode] = useMode();
-  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
-
-  return (
-    <>
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="app">
-            {isDesktop == false && <ProSidebar />}
-            <main className="content">
-              <Topbar />
-              {children}
-            </main>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    </>
-  );
+  return { children };
 }
--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,6 +1,27 @@
 "use client";
-import FormikForm from "./components/FormikForm";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
+import Topbar from "@/app/components/Topbar";
+import ProSidebar from "@/app/components/ProSidebar";
+import { ColorModeContext, useMode } from "@/app/theme";
+import FormikForm from "@/app/components/FormikForm";
 
 export default function Login() {
-  return <FormikForm />;
+  const [theme, colorMode] = useMode();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          {isDesktop == false && <ProSidebar />}
+          <main className="content">
+            <Topbar />
+            <FormikForm />{" "}
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
