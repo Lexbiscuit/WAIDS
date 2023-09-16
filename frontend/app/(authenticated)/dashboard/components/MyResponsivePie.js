e30aa5a65b12994bd073d7d1e4219d702689c892
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-09-16 09:35:52 +0800

    Fixed @nivo/pie color problem.

--- frontend/app/(authenticated)/dashboard/components/MyResponsivePie.js
+++ frontend/app/(authenticated)/dashboard/components/MyResponsivePie.js
@@ -2,6 +2,7 @@
 import { ResponsivePie } from "@nivo/pie";
 import { React, useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
+import { useTheme } from 'next-themes';
 
 const getCategories = async (id) => {
   try {
@@ -18,6 +19,7 @@ const getCategories = async (id) => {
 };
 
 const MyResponsivePie = (props) => {
+  const { theme, setTheme } = useTheme();
   const [logData, setLogData] = useState(null);
 
   useEffect(() => {
@@ -44,28 +46,39 @@ const MyResponsivePie = (props) => {
   }
 
   return (
-      <ResponsivePie
-        data={logData}
-        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-        innerRadius={0.5}
-        padAngle={0.7}
-        cornerRadius={3}
-        activeOuterRadiusOffset={8}
-        borderWidth={1}
-        borderColor={{
-          from: "color",
-          modifiers: [["darker", 0.2]],
-        }}
-        arcLinkLabelsSkipAngle={10}
-        arcLinkLabelsTextColor="#333333"
-        arcLinkLabelsThickness={2}
-        arcLinkLabelsColor={{ from: "color" }}
-        arcLabelsSkipAngle={10}
-        arcLabelsTextColor={{
-          from: "color",
-          modifiers: [["darker", 2]],
-        }}
-      />
+    <ResponsivePie
+      data={logData}
+      colors={{ scheme: "category10" }}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{
+        from: 'color',
+        modifiers: [
+          [
+            'darker',
+            0.2
+          ]
+        ]
+      }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#333333"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: 'color' }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{
+        from: 'color',
+        modifiers: [
+          [
+            'darker',
+            2
+          ]
+        ]
+      }}
+    />
   );
 };
 
