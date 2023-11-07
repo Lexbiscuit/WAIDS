b3a493775f98b0e9fa91a516d204e1c4400514dc
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-11-08 01:11:53 +0800

    Pls I beg be fixed

--- frontend/app/api/idssources/all/route.js
+++ frontend/app/api/idssources/all/route.js
@@ -5,30 +5,30 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 
 export async function GET(request) {
-  //   const session = await getServerSession();
-  //   if (!session) {
-  //     return NextResponse.json(
-  //       { error: "Internal Server Error" },
-  //       { status: 500 }
-  //     );
-  //   }
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 
   try {
     await connectMongoDB();
-    const currentSources = await LogData.distinct("ids_name");
-    const idsSource = await IdsSource.distinct("name");
-    const newSources = currentSources.map((source) => {
-      if (!idsSource.includes(source)) {
-        return source;
-      }
-    });
+    const allSources = await LogData.distinct("ids_name");
 
-    // add the new sources to the IdsSource collection
-    newSources.forEach(async (source) => {
-      if (source) {
+    const currentSources = await IdsSource.distinct("name");
+
+    const newSources = allSources.filter(
+      (source) => !currentSources.includes(source)
+    );
+
+    if (newSources.length > 0) {
+      // add the new sources to the IdsSource collection
+      newSources.map(async (source) => {
         await IdsSource.create({ name: source, isEnabled: true });
-      }
-    });
+      });
+    }
   } catch (e) {
     console.log(e);
   }
